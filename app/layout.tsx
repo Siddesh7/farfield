d7a900b82c71e9815d05f80f6a03cb56b58dfb57
siddesh7 <siddeshsankhya@gmail.com>
2025-07-01 14:07:50 +0530

    viewport zoom fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,6 +63,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon1.png" type="image/png" />
         <meta name="theme-color" content="#000000" />
