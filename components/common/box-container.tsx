196251e4696c6a8c774d2236ace5e1a9e87c4e05
abhishek-01k <abhishekkumar214567@gmail.com>
2025-08-14 15:37:55 +0530

    Removed safe area for box container

--- components/common/box-container.tsx
+++ components/common/box-container.tsx
@@ -1,5 +1,4 @@
-import React, { ReactNode, useEffect, useState } from 'react';
-import { sdk } from '@farcaster/frame-sdk';
+import React, { ReactNode } from 'react';
 
 const BoxContainer = ({
     children,
@@ -8,23 +7,8 @@ const BoxContainer = ({
     children: ReactNode;
     className?: string
 }) => {
-    const [insets, setInsets] = useState({ top: 0, bottom: 15, left: 0, right: 0 });
-
-    useEffect(() => {
-        (async () => {
-            const { client } = await sdk.context;
-            if (client.safeAreaInsets) {
-                setInsets(client.safeAreaInsets);
-            }
-        })();
-    }, []);
-
     return (
         <div
-            style={{
-                paddingBottom: insets.bottom,
-                background: '#fff',
-            }}
             className={`${className}`}
 
         >
